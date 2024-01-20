2676aa03a6ea78bcfb3c68fd1a9627a165a81afa
aussiearef <arefkr@gmail.com>
2024-01-21 06:15:37 +1100

    Upgraded to .NET 8.0

--- Cookie-Based-Roles-Claims/IdentityNetCore/Controllers/IdentityController.cs
+++ Cookie-Based-Roles-Claims/IdentityNetCore/Controllers/IdentityController.cs
@@ -1,131 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using IdentityNetCore.Models;
-using IdentityNetCore.Service;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IdentityNetCore.Controllers
+namespace IdentityNetCore.Controllers;
+
+public class IdentityController(
+    UserManager<IdentityUser> userManager,
+    SignInManager<IdentityUser> signInManager,
+    RoleManager<IdentityRole> roleManager,
+    IUrlHelper urlHelper)
+    : Controller
 {
-    public class IdentityController : Controller
+    public Task<IActionResult> Signup()
     {
+        var model = new SignupViewModel { Role = "Member" };
+        return Task.FromResult<IActionResult>(View(model));
+    }
 
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly IEmailSender emailSender;
-
-        public IdentityController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, IEmailSender emailSender)
+    [HttpPost]
+    public async Task<IActionResult> Signup(SignupViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            _userManager = userManager;
-            this._signInManager = signInManager;
-            this._roleManager = roleManager;
-            this.emailSender = emailSender;
-        }
-        public async Task<IActionResult> Signup()
-        {
-            var model = new SignupViewModel() {Role = "Member" };
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Signup(SignupViewModel model)
-        {
-            if (ModelState.IsValid)
+            if (!await roleManager.RoleExistsAsync(model.Role))
             {
-                if (!(await _roleManager.RoleExistsAsync(model.Role)))
+                var role = new IdentityRole { Name = model.Role };
+                var roleResult = await roleManager.CreateAsync(role);
+                if (!roleResult.Succeeded)
                 {
-                    var role = new IdentityRole {Name = model.Role };
-                    var roleResult = await _roleManager.CreateAsync(role);
-                    if (!roleResult.Succeeded)
-                    {
-                        var errors = roleResult.Errors.Select(s => s.Description);
-                        ModelState.AddModelError("Role", string.Join("," , errors));
-                        return View(model);
-                    }
-                }
-
-
-                if ((await _userManager.FindByEmailAsync(model.Email)) == null)
-                {
-                    var user = new IdentityUser { 
-                        Email= model.Email,
-                        UserName = model.Email
-                    };
-
-                   var result = await _userManager.CreateAsync(user, model.Password);
-                    if (result.Succeeded)
-                    {
-                        var claim = new Claim("Department", model.Department);
-                        await _userManager.AddClaimAsync(user, claim);
-                        await _userManager.AddToRoleAsync(user, model.Role);
-                        return RedirectToAction("Signin");
-                    }
-
-                    ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
+                    var errors = roleResult.Errors.Select(s => s.Description);
+                    ModelState.AddModelError("Role", string.Join(",", errors));
                     return View(model);
                 }
             }
 
-            return View(model);
-        }
 
-
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
-        {
-            var user = await _userManager.FindByIdAsync(userId);
-
-            var result =  await _userManager.ConfirmEmailAsync(user, token);
-            if (result.Succeeded)
+            if (await userManager.FindByEmailAsync(model.Email) == null)
             {
-                return RedirectToAction("Signin");
-            }
+                var user = new IdentityUser
+                {
+                    Email = model.Email,
+                    UserName = model.Email
+                };
 
-            return new NotFoundResult();
-        }
-        public IActionResult Signin()
-        {
-            return View(new SigninViewModel());
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Signin(SigninViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+                var result = await userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-
-                    var user = await _userManager.FindByEmailAsync(model.Username);
-
-                    var userClaims = await _userManager.GetClaimsAsync(user);
-
-                    if (await _userManager.IsInRoleAsync(user, "Member"))
-                    {
-                        return RedirectToAction("Member", "Home");
-                    }
+                    var claim = new Claim("Department", model.Department);
+                    await userManager.AddClaimAsync(user, claim);
+                    await userManager.AddToRoleAsync(user, model.Role);
+                    return RedirectToAction("Signin");
                 }
-                else
-                {
-                    ModelState.AddModelError("Login", "Cannot login.");
-                }
-            }
+
+                ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
                 return View(model);
+            }
         }
 
-        public async Task<IActionResult> AccessDenied()
+        return View(model);
+    }
+
+
+    public async Task<IActionResult> ConfirmEmail(string userId, string token)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+
+        var result = await userManager.ConfirmEmailAsync(user, token);
+        if (result.Succeeded) return RedirectToAction("Signin");
+
+        return new NotFoundResult();
+    }
+
+    public IActionResult Signin()
+    {
+        return View(new SigninViewModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Signin(SigninViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
+            var result =
+                await signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                var user = await userManager.FindByEmailAsync(model.Username);
+
+                var userClaims = await userManager.GetClaimsAsync(user);
+
+                if (await userManager.IsInRoleAsync(user, "Member")) return RedirectToAction("Member", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("Login", "Cannot login.");
+            }
         }
 
-        public async Task<IActionResult> Signout()
+        return View(model);
+    }
+
+    public Task<IActionResult> AccessDenied()
+    {
+        return Task.FromResult<IActionResult>(View());
+    }
+
+    public async Task<IActionResult> Signout()
+    {
+        // For custom sign out, use the code below
+        // await signInManager.SignOutAsync();
+        // return RedirectToAction("Signin");
+        
+        // or use the code below to let ASP.NET Core take care of sign-out
+        return base.SignOut(new AuthenticationProperties
         {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Signin");
-        }
+            RedirectUri = urlHelper.ActionLink("Signin")
+        });
     }
 }
--- Cookie-Based-Roles-Claims/IdentityNetCore/Program.cs
+++ Cookie-Based-Roles-Claims/IdentityNetCore/Program.cs
@@ -1,26 +1,96 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IdentityNetCore.Data;
+using IdentityNetCore.Service;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace IdentityNetCore
+namespace IdentityNetCore;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        CreateHostBuilder(args).Build().Run();
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connString = Configuration["ConnectionStrings:Default"];
+        services.AddDbContext<ApplicationDBContext>(o => o.UseSqlServer(connString));
+        services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDBContext>();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequiredLength = 3;
+            options.Password.RequireDigit = true;
+            options.Password.RequireNonAlphanumeric = false;
+
+            options.Lockout.MaxFailedAccessAttempts = 3;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+            options.SignIn.RequireConfirmedEmail = false;
+        });
+
+        services.ConfigureApplicationCookie(option =>
+        {
+            option.LoginPath = "/Identity/Signin";
+            option.AccessDeniedPath = "/Identity/AccessDenied";
+            option.ExpireTimeSpan = TimeSpan.FromHours(10);
+        });
+
+        services.Configure<SmtpOptions>(Configuration.GetSection("Smtp"));
+
+        services.AddSingleton<IEmailSender, SmtpEmailSender>();
+        services.AddAuthorization(option =>
+        {
+            option.AddPolicy("MemberDep", p => { p.RequireClaim("Department", "Tech").RequireRole("Member"); });
+
+            option.AddPolicy("AdminDep", p => { p.RequireClaim("Department", "Tech").RequireRole("Admin"); });
+        });
+        services.AddControllersWithViews();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        else
+            app.UseExceptionHandler("/Home/Error");
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                "default",
+                "{controller=Home}/{action=Index}");
+        });
     }
 }
